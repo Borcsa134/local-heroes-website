6b845ac008e1dbeb4c2856c6b21c10f00216b635
Borcsa134 <snakekid13@gmail.com>
2024-04-17 19:42:44 +0000

    add discord widget

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
+
+import DiscordWidget from '@/components/discordWidget';
 export default function Home() {
   return (
     <div>
       <h1 className="text-4xl text-center py-4">Üdvözlünk megújult honlapunkon!</h1>
-      <div className="flex justify-center pt-4">
+      <div className="flex justify-around pt-4">
         <Image src="/images/lh30-gyMT.png" alt="Hero image" width={400} height={400} />
+        <DiscordWidget />
       </div>
     </div>
   );
---
+++ components/discordWidget.tsx
@@ -0,0 +1,11 @@
+export default function DiscordWidget() {
+  return (
+    <iframe
+      src="https://discord.com/widget?id=444497157995888640&theme=dark"
+      width="350"
+      height="500"
+      sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+      className="rounded-md hidden md:block"
+    ></iframe>
+  );
+}
