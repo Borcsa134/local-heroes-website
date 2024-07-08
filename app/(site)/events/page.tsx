56acaa106dd68ddd7511e27f018ae364d2e07245
Borisz Juhasz <borisz.juhasz@emarsys.com>
2024-07-08 21:33:28 +0200

    add new event badge style, render other events as well

--- app/(site)/events/page.tsx
+++ app/(site)/events/page.tsx
@@ -1,25 +1,50 @@
+import { OstDocument } from 'outstatic';
 import { getDocuments } from 'outstatic/server';
 
 import EventBadge from '@/components/eventBadge';
 
+type Events = OstDocument<{
+  [key: string]: unknown;
+}>;
+
 async function getData() {
-  const events = getDocuments('events', ['title', 'content', 'slug', 'coverImage', 'publishedAt', 'discordChannel']);
+  const events = getDocuments('events', [
+    'title',
+    'content',
+    'slug',
+    'coverImage',
+    'publishedAt',
+    'discordChannel',
+    'regularEvent',
+  ]);
 
   return events;
 }
 
-async function getRegularEvents() {
-  const events = await getData();
-  return events.filter((event) => event.slug.includes('allando'));
+async function getRegularEvents(events: Events[]) {
+  return events.filter((event) => event.regularEvent === 'true');
+}
+
+async function getOtherEvents(events: Events[]) {
+  return events.filter((event) => event.regularEvent === '');
 }
 
 export default async function Events() {
-  const regularEvents = await getRegularEvents();
+  const events = await getData();
+  const regularEvents = await getRegularEvents(events);
+  const otherEvents = await getOtherEvents(events);
+
   return (
     <div className="lg:w-[990px]">
       <h1 className="text-4xl text-center py-4">Állandó események</h1>
       <div className="flex flex-col md:flex-row justify-around">
         {regularEvents.map((event) => (
+          <EventBadge key={event.slug} event={event} />
+        ))}
+      </div>
+      <h1 className="text-4xl text-center py-4">Közelgő események</h1>
+      <div className="flex flex-col md:flex-row justify-center">
+        {otherEvents.map((event) => (
           <EventBadge key={event.slug} event={event} />
         ))}
       </div>
--- components/eventBadge.tsx
+++ components/eventBadge.tsx
@@ -1,4 +1,5 @@
 import { Card, CardBody } from '@nextui-org/card';
+import { marked } from 'marked';
 import { OstDocument } from 'outstatic';
 
 type Props = {
@@ -8,15 +9,29 @@ type Props = {
   key: string;
 };
 
+function toNormalString(mdString: string) {
+  return marked.parse(mdString.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ''));
+}
+
 export default function EventBadge(props: Props) {
   const { event } = props;
   return (
-    <Card className="py-3 mb-4 md:max-w-[45%]">
+    <Card
+      className="py-3 mb-4 md:max-w-[45%]"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.85), rgba(255,255,255,0) 50%, rgba(0,0,0,0.85) 100%), url(${event.coverImage})`,
+        backgroundSize: 'cover',
+      }}
+    >
       <CardBody className="flex flex-row overflow-visible justify-between py-0 min-h-[120px] max-h-[120px]">
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-between">
           <p className="text-xl md:text-2xl uppercase font-bold pb-2">{event.title}</p>
-          <p className="italic pb-2">{event.content}</p>
-          <p className="text-sm sm:text-base">Discord csatorna: {event.discordChannel as string}</p>
+          <div>
+            <div dangerouslySetInnerHTML={{ __html: toNormalString(event.content) }}></div>
+            {(event.discordChannel as string) && (
+              <p className="text-sm sm:text-base">Discord csatorna: {event.discordChannel as string}</p>
+            )}
+          </div>
         </div>
       </CardBody>
     </Card>
