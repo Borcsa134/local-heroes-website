94a1b6232c87bb7c76d3deb5d17062a52c41ea9f
Borcsa <borisz@juhasz.dev>
2025-04-20 22:57:12 +0200

    fix(outstatic): add missing migrate diffs

--- app/(cms)/outstatic/[[...ost]]/page.tsx
+++ app/(cms)/outstatic/[[...ost]]/page.tsx
@@ -1,8 +1,9 @@
 import { Outstatic } from 'outstatic';
 import { OstClient } from 'outstatic/client';
 import 'outstatic/outstatic.css';
-
-export default async function Page({ params }: { params: { ost: string[] } }) {
-  const ostData = await Outstatic();
-  return <OstClient ostData={ostData} params={params} />;
+ 
+export default async function Page(props: { params: Promise<{ ost: string[] }> }) {
+  const params = await props.params;
+  const ostData = await Outstatic()
+  return <OstClient ostData={ostData} params={params} />
 }
