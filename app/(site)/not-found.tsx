b9b80735b19bcc7792529a89309698013e9b5ba1
Borcsa <borisz@juhasz.dev>
2025-06-29 18:56:50 +0200

    feat(page): add custom 404 page

---
+++ app/(site)/not-found.tsx
@@ -0,0 +1,8 @@
+export default async function NotFound() {
+  return (
+    <div className="flex flex-col justify-center min-h-[600px]">
+      <h1 className="text-4xl text-center py-4">404</h1>
+      <p className="text-center">A keresett oldal nem található</p>
+    </div>
+  );
+}
