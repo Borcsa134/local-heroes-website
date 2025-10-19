93bde3d4761408a9ea53c3d623efd922cfe0cfff
Borcsa <borisz@juhasz.dev>
2025-10-19 19:03:28 +0200

    feat(oauth): only allow discord users to login

--- app/(site)/profile/page.tsx
+++ app/(site)/profile/page.tsx
@@ -1,33 +1,47 @@
 import { Image } from '@heroui/image';
-import { Session } from 'next-auth';
+import { DiscordUsers } from '@prisma/client';
 
 import ProfileForm from '@/app/components/profileForm';
 import { upsertUser } from '@/app/components/upsertUser';
 import { auth } from '@/auth';
 import db from '@/lib/prisma';
 
-async function insertUser(session: Session) {
+async function insertUser(user: DiscordUsers) {
   const formData = new FormData();
-  formData.append('username', session.user.name);
-  formData.append('email', session.user.email);
-  formData.append('fullname', '');
+  formData.append('username', user.username);
+  formData.append('email', user.email);
+  formData.append('fullname', user.fullname);
   await upsertUser(null, formData);
+}
+
+async function isLocalHeroesServerMember(session) {
+  const lhServerProfile = await fetch('https://discord.com/api/users/@me/guilds/444497157995888640/member', {
+    headers: { Authorization: `Bearer ${session?.accessToken}` },
+  });
+  return lhServerProfile.status == 200 ? true : false;
 }
 
 export default async function Profile() {
   const session = await auth();
+  let user = null;
 
   if (session?.user?.name && session.user.email) {
-    let user = await db.discordUsers.findFirst({ where: { email: session.user.email } });
+    user = await db.discordUsers.findFirst({ where: { email: session.user.email } });
 
-    if (!(user?.username && user?.email)) {
-      await insertUser(session);
-      user = await db.discordUsers.findFirst({ where: { email: session.user.email } });
+    if (!user && (await isLocalHeroesServerMember(session))) {
+      user = {
+        username: session.user.name,
+        email: session.user.email,
+        fullname: '',
+      };
+      await insertUser(user as DiscordUsers);
     }
+  }
 
-    return (
-      <div>
-        <h1 className="text-4xl text-center py-4">Profil</h1>
+  return (
+    <div>
+      <h1 className="text-4xl text-center py-4">Profil</h1>
+      {user && (
         <div className="flex flex-col items-center sm:flex-row sm:justify-between px-10">
           <Image
             removeWrapper
@@ -38,16 +52,17 @@ export default async function Profile() {
           />
           <ProfileForm username={user.username} email={user.email} fullname={user.fullname} />
         </div>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h1 className="text-4xl text-center py-4">Profil</h1>
-      <div className="">
-        <p className="italic text-center text-gray-500">Nem vagy bejelentkezve.</p>
-      </div>
+      )}
+      {!user && session && (
+        <div className="">
+          <p className="italic text-center text-gray-500">Nem vagy tagja a Local Heroes Discord szervernek.</p>
+        </div>
+      )}
+      {!user && !session && (
+        <div className="">
+          <p className="italic text-center text-gray-500">Nem vagy bejelentkezve.</p>
+        </div>
+      )}
     </div>
   );
 }
--- auth.ts
+++ auth.ts
@@ -1,7 +1,30 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
 import Discord from 'next-auth/providers/discord';
 
+declare module 'next-auth' {
+  interface Session {
+    accessToken: string;
+    user: DefaultSession['user'];
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord],
+  providers: [
+    Discord({
+      authorization: 'https://discord.com/api/oauth2/authorize?scope=identify+email+guilds.members.read',
+    }),
+  ],
+  callbacks: {
+    jwt({ token, account }) {
+      if (account?.provider === 'discord') {
+        return { ...token, accessToken: account.access_token };
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
   trustHost: true,
 });
